9074ec64e76ca99efa963c4f711285d2cb6b64d3
Francis Mukuha <mukuha58@gmail.com>
2019-07-22 20:02:20 +0300

    edit database

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'insta',
+    'insta.apps.InstaConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
