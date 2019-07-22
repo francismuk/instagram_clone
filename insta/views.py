313779c32b1b4e012774fc3e70153505f5eecd59
Francis Mukuha <mukuha58@gmail.com>
2019-07-22 20:20:05 +0300

    add subscribe form to views.index

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse, Http404
 import datetime as dt
+from .forms import SubscribeForm
 
 # Create your views here.
 def index(request):
     title = 'Home'
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = SubscribeForm()
 
-    return render(request,'index.html', {'title':title})
+    return render(request,'index.html', {'title':title, 'letterForm':form, })
