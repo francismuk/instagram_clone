65e005577ec608b41a24bf0ad069d89b858f844b
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 16:46:49 +0300

    edit views.py

--- insta/views.py
+++ insta/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
+from django.core.urlresolvers import reverse
 from .email import send_welcome_email
 from .forms import SubscribeForm, NewPostForm
+from django.contrib.auth.models import User
 from .models import Subscriber, Image, Location, Category
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     title = 'Home'
-    images = Image.get_all_images()
     if request.GET.get('location'):
         images = Image.filter_by_location(request.GET.get('location'))
 
-    elif request.GET.get('tags'):
-        images = Image.filter_by_tag(request.GET.get('tags'))
-
-    # elif request.GET.get('search_term'):
-    #     images = Image.search_image(request.GET.get('search_term'))
-
+    
     else:
         images = Image.objects.all()
         
