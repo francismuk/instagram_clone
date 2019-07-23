4ffa55929654acf0b9319baffa0695570aa9635a
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 10:30:42 +0300

    add new post form

--- insta/forms.py
+++ insta/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from .models import Image
 
 class SubscribeForm(forms.Form):
+    name = forms.CharField(label='First Name', max_length=30)
+    email = forms.EmailField(label = 'Email')
+    
+class NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['poster', 'post_date']
         widgets = {
             'tags': forms.CheckboxSelectMultiple(),
         }
-        
-    name = forms.CharField(label='First Name', max_length=30)
-    email = forms.EmailField(label = 'Email')
