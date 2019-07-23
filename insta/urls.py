279b94c1cb9587238eb792b656514c0a4406059b
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 11:22:38 +0300

    add registration urls

--- insta/urls.py
+++ insta/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name='index'),
+    url(r'^new/post$', views.new_post, name='new-post')
 ]
 # if settings.DEBUG:
 #     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
 from .email import send_welcome_email
-from .forms import SubscribeForm
+from .forms import SubscribeForm, NewPostForm
 from .models import Subscriber, Image, Location, Category
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -22,3 +22,19 @@ def index(request):
         form = SubscribeForm()
 
     return render(request,'index.html', {'title':title, 'letterForm':form })
+
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.poster = current_user
+            image.save()
+        return redirect('index')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
