c18beb597209a18a07fe8d0ce347ce43f88941f3
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 10:02:04 +0300

    edit viesw.py

--- insta/views.py
+++ insta/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
+from .email import send_welcome_email
 from .forms import SubscribeForm
-
+from .models import Subscriber, Image, Location, Category
 # Create your views here.
 def index(request):
     title = 'Home'
     if request.method == 'POST':
         form = SubscribeForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['subscriber']
+            name = form.cleaned_data['name']
             email = form.cleaned_data['email']
-
-            recipient = SubscribeForm(name = name,email =email)
+            recipient = Subscriber(name=name,email=email)
             recipient.save()
             send_welcome_email(name,email)
+            HttpResponseRedirect('index', {"name":name})
+            
+    else:
+        form = SubscribeForm()
 
-    return render(request,'index.html', {'title':title, 'letterForm':form, })
+    return render(request,'index.html', {'title':title, 'letterForm':form })
