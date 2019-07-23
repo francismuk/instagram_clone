a9bf179cf1dd4645bcba9201b0842fcd03d105ba
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 15:20:15 +0300

    add media static settings

--- insta/urls.py
+++ insta/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     url('^$',views.index,name='index'),
     url(r'^new/post$', views.new_post, name='new-post')
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
