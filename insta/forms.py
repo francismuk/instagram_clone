733def98babcf6bf79863499d694b8722e4255a1
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 10:01:51 +0300

    edit forms

--- insta/forms.py
+++ insta/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class SubscribeForm(forms.Form):
-    subscriber = forms.CharField(label='First Name', max_length=30)
+    name = forms.CharField(label='First Name', max_length=30)
     email = forms.EmailField(label = 'Email')
