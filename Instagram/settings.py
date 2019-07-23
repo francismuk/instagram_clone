9988acddb59b9574f086541d3200527befb1914f
Francis Mukuha <mukuha58@gmail.com>
2019-07-23 10:28:17 +0300

    add tinymce settings

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'insta.apps.InstaConfig',
     'bootstrap3',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
