f18f98e2d12ee876bb0487d9b0cde36c4f6c4614
Francis Mukuha <mukuha58@gmail.com>
2019-07-24 14:37:37 +0300

    edit index function

--- insta/views.py
+++ insta/views.py
@@ -2,13 +2,31 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
 from .email import send_welcome_email
-from .forms import SubscribeForm, NewPostForm
+from .forms import SubscribeForm, NewPostForm, CommentForm
 from django.contrib.auth.models import User
-from .models import Subscriber, Image, Location, Category
+from .models import Subscriber, Image, Location, Category, Comments
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     title = 'Home'
+    current_user = request.user
+    comments = Comments.get_comment(Comments, id)
+    if request.method == 'POST':
+        formc = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.cleaned_data['comment']
+
+            comment = Comment()
+            comment.image = image
+            comment.user = current_user
+            comment.comment = comment
+            comment.save()
+
+    else:
+        formc = CommentForm()
+
+    
+    
     if request.GET.get('location'):
         images = Image.filter_by_location(request.GET.get('location'))
 
@@ -29,7 +47,7 @@ def index(request):
     else:
         form = SubscribeForm()
 
-    return render(request,'index.html', {'title':title, 'images':images, 'letterForm':form })
+    return render(request,'index.html', {'title':title, 'images':images, 'letterForm':form, 'commentForm':formc, 'comments':comments })
 
 
 @login_required(login_url='/accounts/login/')
