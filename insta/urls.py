015484bcee050e65b84f797e8b932649b628b8c5
Francis Mukuha <mukuha58@gmail.com>
2019-07-25 11:47:06 +0300

    add profile url

--- insta/urls.py
+++ insta/urls.py
@@ -7,7 +7,8 @@ urlpatterns=[
     url('^$',views.index,name='index'),
     url(r'^new/post$', views.new_post, name='new-post'),
     url(r'^image/(\d+)',views.image,name ='image'),
-    url(r'^profile$', views.profile, name='profile'),
+    # url(r'^search/', views.search_users, name='search_users'),
+    url(r'^profile/(?P<username>[0-9]+)$', views.profile_pages, name='profile_pages'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
