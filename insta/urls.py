c8d7ee7cc6f79e42f94ea825b5036782ebc22e91
Francis Mukuha <mukuha58@gmail.com>
2019-07-29 11:24:29 +0300

    add search url

--- insta/urls.py
+++ insta/urls.py
@@ -7,8 +7,12 @@ urlpatterns=[
     url('^$',views.index,name='index'),
     url(r'^new/post$', views.new_post, name='new-post'),
     url(r'^image/(\d+)',views.image,name ='image'),
-    # url(r'^search/', views.search_users, name='search_users'),
-    url(r'^profile/(?P<username>[0-9]+)$', views.profile_pages, name='profile_pages'),
+    url(r'^search/', views.search, name='search'),
+    url(r'^profile/(?P<username>[0-9]+)$', views.profile, name='users_page'),
+    url(r'^mainprofile/$', views.mainprofile, name='mainprofile'),
+    url(r'^edit/profile$', views.edit_user, name='edit_user'),
+
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
